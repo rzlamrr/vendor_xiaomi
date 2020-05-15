fbe3d956ad35cacb70d9699dcbe88cfdbaee0f7a
PainKiller3 <ninadpatil100@gmail.com>
2020-05-15 09:54:22 +0530

    sdm845-common: Update vulkan & egl drivers
    * Updated vulkan & egl drivers to v464.0
    * Extracted from https://forum.xda-developers.com/poco-f1/development/driver-adreno-600-series-opengl-vulkan-t4089967
    * Special thanks to Arda Süzen for updating the drivers
    
    Change-Id: Ia10e279d0c7c762847fab36c6f4b4ffbed9e4212
    
    Conflicts:
    	sdm845-common/proprietary/vendor/lib/egl/libGLESv2_adreno.so
    	sdm845-common/proprietary/vendor/lib/hw/vulkan.sdm845.so
    	sdm845-common/proprietary/vendor/lib/libCB.so
    	sdm845-common/proprietary/vendor/lib/librs_adreno_sha1.so
    	sdm845-common/proprietary/vendor/lib64/egl/libGLESv2_adreno.so
    	sdm845-common/proprietary/vendor/lib64/hw/vulkan.sdm845.so
    	sdm845-common/proprietary/vendor/lib64/libCB.so
    	sdm845-common/proprietary/vendor/lib64/librs_adreno_sha1.so
    
    Conflicts:
    	sdm845-common/proprietary/vendor/lib/egl/libGLESv2_adreno.so
    	sdm845-common/proprietary/vendor/lib/hw/vulkan.sdm845.so
    	sdm845-common/proprietary/vendor/lib/libCB.so
    	sdm845-common/proprietary/vendor/lib/librs_adreno_sha1.so
    	sdm845-common/proprietary/vendor/lib64/egl/libGLESv2_adreno.so
    	sdm845-common/proprietary/vendor/lib64/hw/vulkan.sdm845.so
    	sdm845-common/proprietary/vendor/lib64/libCB.so
    	sdm845-common/proprietary/vendor/lib64/librs_adreno_sha1.so

--- sdm845-common/sdm845-common-vendor.mk
+++ sdm845-common/sdm845-common-vendor.mk
@@ -292,6 +292,11 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/sdm845-common/proprietary/vendor/etc/perf/targetconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetconfig.xml \
     vendor/xiaomi/sdm845-common/proprietary/vendor/etc/perf/targetresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetresourceconfigs.xml \
     vendor/xiaomi/sdm845-common/proprietary/vendor/etc/permissions/vendor-qti-hardware-sensorscalibrate.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor-qti-hardware-sensorscalibrate.xml \
+    vendor/xiaomi/sdm845-common/proprietary/vendor/etc/permissions/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
+    vendor/xiaomi/sdm845-common/proprietary/vendor/etc/permissions/android.hardware.vulkan.compute.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml \
+    vendor/xiaomi/sdm845-common/proprietary/vendor/etc/permissions/android.hardware.vulkan.level.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
+    vendor/xiaomi/sdm845-common/proprietary/vendor/etc/permissions/android.hardware.vulkan.version.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml \
+    vendor/xiaomi/sdm845-common/proprietary/vendor/etc/permissions/android.software.vulkan.deqp.level.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
     vendor/xiaomi/sdm845-common/proprietary/vendor/etc/scve/facereco/gModel.dat:$(TARGET_COPY_OUT_VENDOR)/etc/scve/facereco/gModel.dat \
     vendor/xiaomi/sdm845-common/proprietary/vendor/etc/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config \
     vendor/xiaomi/sdm845-common/proprietary/vendor/etc/wfdconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/wfdconfig.xml \
